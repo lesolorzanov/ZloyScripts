f05a6ac87d4fcfa4974cb2a7b8e052c3fbc72048
Leslie Evelyn Solorzano <zloysmiertniy@users.noreply.github.com>
2017-05-19 11:19:32 +0200

    ssh command

--- abitofeverything.sh
+++ abitofeverything.sh
@@ -1,2 +1,5 @@
 #run git pull in all subfolders from where command is ran
 find . -maxdepth 1 -type d -print -execdir git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
+
+#ssh without losing connection
+ssh -o TCPKeepAlive=yes -o ServerAliveInterval=50
